047b2116b784b88e0a1f1c76431060be674c8674
joker69216 <hamaraad883@gmail.com>
2022-12-05 23:13:09 +0300

    add data to the table

--- data.sql
+++ data.sql
@@ -1,5 +1,17 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts,
+          neutered, weight_kg) 
+          VALUES (0, 'Agumon', '3-FEB-2020', 0, TRUE, 10.23)
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts,
+          neutered, weight_kg) 
+          VALUES (1, 'Gabumon', '15-NOV-2018', 2, TRUE, 8)
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts,
+          neutered, weight_kg) 
+          VALUES (2, 'Pikachu', '7-JAN-2021', 1, FALSE, 15.04)
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts,
+          neutered, weight_kg) 
+          VALUES (3, 'Devimon', '12-MAY-2017', 5, TRUE, 11)
