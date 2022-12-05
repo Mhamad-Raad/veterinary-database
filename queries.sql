8b71fb43402361cd5b438ca575053050fe436fbd
joker69216 <hamaraad883@gmail.com>
2022-12-05 23:47:39 +0300

    add the needed queries to the system

--- queries.sql
+++ queries.sql
@@ -1,3 +1,16 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '1-FEB-2016'
+AND '12-31-2019'
+
+SELECT * FROM animals WHERE neutered = TRUE AND escape_attempts < 3
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu'
+
+SELECT name, escape_attempts FROM animals where weight_kg > 10.5
+
+SELECT * FROM animals where neutered = TRUE
+
+SELECT * FROM animals WHERE name != 'Gabumon'
+
+SELECT * FROM animals WHERE weight_kg <= 17.3 AND weight_kg >= 10.4
