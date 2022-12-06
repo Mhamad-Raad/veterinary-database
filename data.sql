817babae60678131bb80b7e71038722acc640a76
joker69216 <hamaraad883@gmail.com>
2022-12-06 18:13:13 +0300

    add new data to the table

--- data.sql
+++ data.sql
@@ -15,3 +15,13 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts,
 INSERT INTO animals (id, name, date_of_birth, escape_attempts,
           neutered, weight_kg) 
           VALUES (3, 'Devimon', '12-MAY-2017', 5, TRUE, 11)
+
+insert into animals (id, name, date_of_birth, escape_attempts,
+          neutered, weight_kg) 
+          VALUES (4, 'Charmander', '8-FEB-2020', 0, FALSE, 11),
+          (5, 'Plantmon', '8-FEB-2020', 2, TRUE, 5.7),
+          (6, 'Squirtle', '2-APR-1993', 3, FALSE, 12.13),
+          (7, 'Angemon', '12-JUN-2005', 1, TRUE, 45),
+          (8, 'Boarmon', '7-JUN-2005', 7, TRUE, 20.4 ),
+          (9, 'Blossom', '13-OCT-1998', 0, TRUE, 17 ),
+          (10, 'Ditto', '14-MAY-2022', 4, TRUE, 22 )
